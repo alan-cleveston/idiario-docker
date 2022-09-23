aeb71a7a18d8dcbb683c54c006a5a4a680ac4215
alan.cleveston <alan.cleveston@softagon.com.br>
2022-09-23 10:56:20 -0300

    Separando postgres

--- i-diario/src/entrypoint-2.sh
+++ i-diario/src/entrypoint-2.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-#POSTGRES
-#POSTGRES_USER=${POSTGRES_USER:-postgres}
-POSTGRES_USER="postgres"
-POSTGRES_DATABASE=${POSTGRES_DATABASE:-idiario}
-
-PG_CMD_START="/usr/lib/postgresql/12/bin/pg_ctl start -D $PGDATA -l $PGDATA/postgresql.log"
-
-if [ -e $PGDATA/PG_VERSION ]; then
- echo "$PGDATA inicializado!!!"
- su - postgres -c "$PG_CMD_START"
-else
- if [ -z "$POSTGRES_PASSWORD" ]; then
-  echo "ENV POSTGRES_PASSWORD vazia!!!"
- else
-  chown -R postgres:postgres /var/lib/postgresql
-  su - postgres -c "/usr/lib/postgresql/12/bin/initdb $PGDATA --encoding=UTF-8 --lc-collate=C --lc-ctype=C"
-  echo "host all  all    0.0.0.0/0  md5" >> $PGDATA/pg_hba.conf
-  sed -i "s/#listen_addresses/listen_addresses/" $PGDATA/postgresql.conf
-  #su - postgres -c "$PG_CMD_START && psql --command \"ALTER USER $POSTGRES_USER WITH ENCRYPTED PASSWORD '$POSTGRES_PASSWORD';\" && psql --command \"CREATE DATABASE $POSTGRES_DATABASE\""
-  su - postgres -c "$PG_CMD_START && psql --command \"ALTER USER $POSTGRES_USER WITH ENCRYPTED PASSWORD '$POSTGRES_PASSWORD';\""
- fi
-fi
-tail -F $PGDATA/postgresql.log &
-
 #REDIS
 echo "Iniciando Redis... na porta: $REDIS_PORT"
 #echo -e 'daemonize yes\nlogfile /tmp/redis.log\nrequirepass $REDIS_PASS\nport $REDIS_PORT' > /etc/redis.conf
@@ -36,13 +12,14 @@ echo "Memcached..."
 memcached -l 127.0.0.1 -d -u root
 
 #Configurando config/database.sample.yml
-sed -i "s;username:.*;username: $POSTGRES_USER;" config/database.sample.yml
-sed -i "s;password:.*;password: $POSTGRES_PASSWORD;" config/database.sample.yml
-sed -i "s;database:.*;database: $POSTGRES_DATABASE;" config/database.sample.yml
+sed -i "s;username:.*;username: $IDIARIO_DB_USER;" config/database.sample.yml
+sed -i "s;password:.*;password: $IDIARIO_DB_PASS;" config/database.sample.yml
+sed -i "s;database:.*;database: $IDIARIO_DB_NAME;" config/database.sample.yml
+sed -i "s;host:.*;host: $IDIARIO_DB_HOST;" config/database.sample.yml
 echo -e "
 production:
   <<: *default
-  database: $POSTGRES_DATABASE" >> config/database.sample.yml
+  database: $IDIARIO_DB_NAME" >> config/database.sample.yml
 
 # Conf config/initializers/*.rb
 echo "Configurando config/initializers/*"
